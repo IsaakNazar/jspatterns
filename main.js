94a9fd608461267d1457f266e1d0fe66d9d3e4f0
nisakov <nazarbek.isakov@zensoft.io>
2019-04-01 09:29:20 +0600

    mediator pattern

--- main.js
+++ main.js
@@ -4,19 +4,22 @@
       pubsub = require("./js/pubsub/init"),
       strategy = require("./js/strategy/init"),
       observer = require("./js/observer/init"),
-      cor = require("./js/cor/init");
+      cor = require("./js/cor/init"),
+      mediator = require("./js/mediator/init");
 
   const examples = {
     factory,
     pubsub,
     strategy,
     observer,
-    cor
+    cor,
+    mediator
   };
   // console.log(examples.factory.init());
   // console.log(examples.pubsub.init());
   // console.log(examples.strategy.init());
   // console.log(examples.observer.init());
-  console.log(examples.cor.init());
+  //console.log(examples.cor.init());
+  console.log(examples.mediator.init());
 
 })();
